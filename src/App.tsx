1e1a32b7bf39475a97d7d3f3be21c6b3e2c0bd47
Jesse Clark <hello@developerjesse.com>
2022-04-14 14:53:02 +0300

    Add rLogin and ability to purchase rDoc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,70 @@
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
-import React from 'react'
+import RLogin from '@rsksmart/rlogin'
+import React, { useState } from 'react'
 import './App.scss'
 
+const rLogin = new RLogin({
+  rpcUrls: {
+    31: 'https://public-node.testnet.rsk.co'
+  },
+  supportedChains: [31]
+})
+
 function App () {
-  const ramp = new RampInstantSDK({
-    // for testnet:
-    url: 'https://ri-widget-staging.firebaseapp.com/',
+  const [provider, setProvider] = useState(null)
+  const [address, setAddress] = useState<string>('')
 
-    // for IOV!
-    // swapAsset: 'RSK_RDOC',
-    userAddress: '0x3dD03d7d6c3137f1Eb7582bA5957B8a2E26f304A'.toLowerCase(), // <-- must be lowercase or checksummed!
-
-    // for the dapp:
-    hostAppName: 'Ramp POC',
-    hostLogoUrl: 'https://rampnetwork.github.io/assets/misc/test-logo.png'
-  })
+  const connectToWallet = () => {
+    rLogin.connect()
+      .then((rLoginResponse: any) => {
+        console.log('response:', rLoginResponse)
+        setProvider(rLoginResponse.provider)
+        rLoginResponse.provider.request({ method: 'eth_accounts' })
+          .then((addresses: string[]) => setAddress(addresses[0]))
+      })
+      .catch(console.log)
+  }
 
   const buyDoc = () => {
-    ramp.show()
+    const ramp = new RampInstantSDK({
+      // for testnet:
+      // url: 'https://ri-widget-staging.firebaseapp.com/',
+      url: 'https://ri-widget-staging.web.app/',
+
+      // for IOV:
+      swapAsset: 'RSK_RDOC',
+      // userAddress must be lowercase or checksummed correctly:
+      userAddress: address.toLowerCase(),
+
+      // for the dapp:
+      hostAppName: 'Ramp POC',
+      hostLogoUrl: 'https://rampnetwork.github.io/assets/misc/test-logo.png'
+    })
+
+    ramp.on('*', event => console.log(event)).show()
   }
 
   return (
     <div className="App">
       <h1>Ramp Demo!</h1>
 
-      <button onClick={buyDoc}>Buy!</button>
+      {!provider && <button onClick={connectToWallet}>Connect!</button>}
+
+      {provider && (
+        <div>
+          <table>
+            <tr>
+              <th>Address</th>
+              <td>{address}</td>
+            </tr>
+            <tr>
+              <th>rDoc Balance</th>
+              <td>0</td>
+            </tr>
+          </table>
+          <button onClick={buyDoc}>Buy RDOC!</button>
+        </div>
+      )}
     </div>
   )
 }
